da4dc709819a093170348b09d78516bbd6d93716
Tran Ninh Hieu <hieukute123a@gmail.com>
2021-04-27 00:18:07 +0700

    tao th2

---
+++ Baith2/src/baith2/Baith2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baith2;
+
+/**
+ *
+ * @author ADMIN
+ */
+class SinhVien{
+    private int mSSV;
+    private String hoVaTen;
+    private double diemGiaiTich;
+    private double diemVatLy;
+    private double diemLapTrinh;
+    private double diemTrungBinh;
+    public void setMSSV(int MSSV){this.mSSV = MSSV;}
+    public void setHoVaTen(String HoVaTen){this.hoVaTen=HoVaTen;}
+    public void setDiemGiaiTich(double DiemGiaiTich){this.diemGiaiTich=DiemGiaiTich;}
+    public void setDiemVatLy(double DiemVatLy){this.diemVatLy=DiemVatLy;}
+    public void setDiemLapTrinh(double DiemLapTrinh){this.diemLapTrinh=DiemLapTrinh;}
+    
+    public int getMSSV(){return mSSV;}
+    public String getHoVaTen(){return hoVaTen;}
+    public double getDiemGiaiTich(){return diemGiaiTich;}
+    public double getDiemVatLy(){return diemVatLy;}
+    public double getDiemLapTrinh(){return diemLapTrinh;}
+    public double 
+}
+public class Baith2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
